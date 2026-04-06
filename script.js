e6bac1c8dce703e1fff526563b3efdad496410a1
Skywing608 <VenableN0879@my.sduhsd.net>
2026-04-06 21:08:17 +0000

    name and difficulty selector

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// add javascript here
+//Finding name
+var name = prompt("Hello! What is your name?");
+
+//difficulty selection
+var easy = document.querySelector('input[id="e"]:checked').value;
+var medium = document.querySelector('input[id="m"]:checked').value;
+var hard = document.querySelector('input[id="h"]:checked').value;
+
+//Play button
+function play(){
+    document.getElementById("guessBtn").disabled = false;
+    if (easy = true){
+        var randomNum = Math.floor(Math.random() * easy) + 1;
+        alert(randomNum);
+    }
+    
+    if (medium = true){
+        var randomNum = Math.floor(Math.random() * medium) + 1;
+        alert(randomNum);
+    }
+    
+    if (hard = true){
+        var randomNum = Math.floor(Math.random() * hard) + 1;
+        alert(randomNum);
+    }
+
+}
+
+document.getElementById("playBtn").addEventListener("click", play);
